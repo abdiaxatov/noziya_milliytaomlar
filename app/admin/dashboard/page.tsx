4d025405f00640b53c6f20a3f0694d49ce1a69ef
Nurbek Abdiaxatov <nurbekabdiaxatov@gmail.com>
2026-01-28 22:26:18 +0500

    main

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,5 +1,9 @@
-import { AdminDashboard } from "@/components/admin/admin-dashboard"
+// import { AdminDashboard } from "@/components/admin/admin-dashboard"
+import { MenuManagement } from "@/components/admin/menu-management"
 
 export default function DashboardPage() {
-  return <AdminDashboard />
+  // return <AdminDashboard />
+  return <MenuManagement />
+
 }
+
